4b0f3164e28be4abb3328f601adc0f3ddc8ac081
Gean <geanfranklinvillamar@gmail.com>
2025-01-30 16:52:53 -0500

    edición en app.js, se elimino una ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router";
 import "./App.css";
 import Home from "./components/Home";
-import ModalUpdate from "./components/ModalUpdate";
 
 function App() {
   return (
     <div className="Application">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/update/:id" element={<ModalUpdate />} />
       </Routes>
     </div>
   );
