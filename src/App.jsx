b771a5b2b5763f665d0dd16a6c429c29a7da5cc5
Gean <geanfranklinvillamar@gmail.com>
2025-01-31 16:27:20 -0500

    modified src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { Routes, Route } from "react-router";
 import "./App.css";
 import Home from "./components/Home";
 
 function App() {
   return (
     <div className="Application">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Home />
     </div>
   );
 }
